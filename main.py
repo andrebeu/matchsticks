a7b9487b6964593283cade41c17cd02360fac5cb
andrebeu <aobeukers@gmail.com>
2021-11-21 10:28:18 -0500

    full BFS implementation

--- main.py
+++ main.py
@@ -6,50 +6,62 @@ def form_obs(sticks):
     x[sticks] = 1
     return x
 
-class Task():
-    def __init__(self,init_state,final_states,max_depth=2):
-        self.s0 = init_state
-        self.sfL = final_states # list 
-        self.max_depth = max_depth
+
 
 class State():
-    def __init__(self,obs,depth,is_final=False):
-        self.is_final = is_final # check if final
+    """ 
+    currently only implements valid action
+    """
+    def __init__(self,obs,is_final):
         self.obs = obs
-        self.depth = depth
         self.va = np.where(obs)[0] # valid actions
+        self.is_final = is_final
 
-    def get_stp(self,action):
-        """ action is index
-        indicates what board entry to zero out
+class Task():
+    def __init__(self,init_state_obs,final_states_obs,max_depth=2):
+        self.x0 = init_state_obs
+        self.xfL = final_states_obs
+        # construct state from obs
+        self.s0 = State(init_state_obs,is_final=False)
+        # self.sfL = [State(xf,is_final=True) for xf in final_states_obs] 
+        self.max_depth = max_depth
+
+    def check_final(self,xt):
+        return np.any([np.all(xt == x) for x in self.xfL])
+
+    def get_stp(self,state_t,action):
+        """ get next state
         """
-        if action not in self.va:
-            assert False, 'A=%i not in %s'%(action,str(self.va))
-        obs_stp = copy(self.obs)
-        obs_stp[action] = 0
-        stp = State(obs=obs_stp,depth=self.depth+1)
+        st = state_t
+        # check valid action
+        va = st.va
+        if action not in va:
+            assert False, 'A=%i not in vA %s'%(action,str(va))
+        # make xtp, and stp
+        xtp = copy(st.obs)
+        xtp[action] = 0
+        xtp_is_final = self.check_final(xtp)
+        stp = State(xtp,is_final=xtp_is_final)
         return stp
 
 
 def BFSPlay(task):
-    s0 = task.s0
-    st = s0 
-    depth = st.depth
+    s0 = st = task.s0
     nsteps = 0
     wsteps = 0
+    depth = 0
     while depth<=task.max_depth:
         wsteps +=1
-        va = st.va
         # check all nodes in current depth
-        for at in va:
+        for at in st.va:
             print('a%i'%nsteps,at)
-            stp = st.get_stp(va)
+            stp = task.get_stp(st,at)
             nsteps +=1
             if stp.is_final:
                 return nsteps
         # if nothing found, step down
         st = stp
-        depth = st.depth
+        depth += 1
         # safety
         if wsteps > 5:
             assert False, 'possible infinite while'
