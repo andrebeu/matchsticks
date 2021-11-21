d6cca8124c293b2c62e14ab96db510e079969b57
andrebeu <aobeukers@gmail.com>
2021-11-21 09:21:54 -0500

    BFSplay done; not runnning

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import numpy as np
 
 
-def play(agent,task):
-    st = stp = task.s0
-    nsteps = 0
-    while not stp.is_final:
-        st,stp = agent.act(st)
-        nsteps +=1
-    return nsteps
+def form_obs(sticks):
+    x = np.zeros(24)
+    x[sticks] = 1
+    return x
 
 class Task():
-    def __init__(self,init_state):
-        s0 = self.init_state
+    def __init__(self,init_state,final_states,max_depth=2):
+        self.s0 = init_state
+        self.sfL = final_states # list 
+        self.max_depth = max_depth
 
 class State():
-    def __init__(self):
-        self.is_final = None
+    def __init__(self,obs):
+        self.is_final = None # check if final
+        self.obs = obs
+        self.va = np.where(obs)[0] # valid actions
 
+    def get_stp(self,action):
+        """ action is index
+        indicates what board entry to zero out
+        """
+        if action not in self.va:
+            assert False, 'A=%i not in %s'%(action,str(self.va))
+        obs = state.obs
+        obs[action] = 0
+        return State(obs)
 
 
 def BFSPlay(task):
     s0 = task.init_state
-    st = stp = s0
-    va = st.valid_actions
+    st = s0
     depth = st.depth
     while depth<=task.max_depth:
         depth = st.depth
+        va = st.valid_actions
         # check all nodes in current depth
         for at in va:
             stp = st.get_stp(va)
