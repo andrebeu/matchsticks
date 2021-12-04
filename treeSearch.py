90a1cb4a0e8d15d22c953517b8f82f7d998508c9
andrebeu <aobeukers@gmail.com>
2021-12-04 14:48:18 -0500

    simple case results+debugg verb flag

--- treeSearch.py
+++ treeSearch.py
@@ -3,43 +3,52 @@ from copy import copy as copy
 import random
 from collections import deque
 
+V = False
 
 def treeSearch(taskL,BFS=True,memory=False):
+    if V: print('\nBFS=',BFS,'mem=',memory)
     sol_nodes = []
     sol_times = []
+    sol_mode = []
     for task in taskL:
+        if V: print('task:')
         nitr = 0
-        deq = deque()
-        nodet = task.head_node
-        deq.append(nodet)
+        deq = deque([task.head_node])
         while len(deq):
-            # print(sol_nodes)
             nitr += 1
-            ## take candidate node
+            ## pop candidate node
             if BFS:
                 nodet = deq.popleft()
             else:
                 nodet = deq.pop()  
-            # print(np.where(nodet.obs))
-            ## eval node
+            if V: print(nodet.valid_actions)
+            ## eval if node is final
             if task.check_final(nodet):
                 sol_nodes.append(nodet)
+                # record RT
                 sol_times.append(nitr)
+                # solution mode no memory
+                sol_mode.append(0)
+                if V: print('sol by search')
                 break
             ## check memory
             elif memory:
                 sol_in_memory = 0
                 for sol_node in sol_nodes:
-                    ## consider recognition failure w.p.p                    
+                    # check tree against memory solutions
                     if sol_node in deq:
-                        ## keep track of number of memory operations
+                        # record RT
                         sol_times.append(nitr+1)
-                        sol_in_memory = 1
+                        sol_in_memory = True
                         break # from memory for
                 if sol_in_memory:
-                    # print('memory sol')
+                    if V: print('sol by memory')
+                    # solution mode = memory
+                    sol_mode.append(1)
                     break # from task while
             ## expand tree
             random.shuffle(nodet.children)
             deq.extend(nodet.children)
-    return sol_times
+    if V: print('cost=',sol_times)
+    return sol_times,sol_mode
+
